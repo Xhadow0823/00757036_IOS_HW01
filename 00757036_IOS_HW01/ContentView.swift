5d97f183b08269753e881fdb902e90856e89ba04
gitHub <jack940197@gmail.com>
2020-09-30 20:15:20 +0800

    Initial Commit

---
+++ 00757036_IOS_HW01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00757036_IOS_HW01
+//
+//  Created by User06 on 2020/9/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00757036_IOS_HW01/_0757036_IOS_HW01App.swift
@@ -0,0 +1,17 @@
+//
+//  _0757036_IOS_HW01App.swift
+//  00757036_IOS_HW01
+//
+//  Created by User06 on 2020/9/30.
+//
+
+import SwiftUI
+
+@main
+struct _0757036_IOS_HW01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
